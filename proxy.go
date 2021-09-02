6bf43c7c4ae7d14036ebc8251d65a8d427b2ee73
Carcraftz <40304589+Carcraftz@users.noreply.github.com>
2021-09-01 22:38:51 -0400

    big update 3.0!!!

--- proxy.go
+++ proxy.go
@@ -1,25 +1,35 @@
 package main
 
 import (
+	"compress/zlib"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
+	"strconv"
+	"time"
+
+	"bytes"
+	"compress/gzip"
 	"net/url"
 	"strings"
 
-	http "github.com/useflyent/fhttp"
+	"github.com/Carcraftz/cclient"
+	"github.com/andybalholm/brotli"
 
-	"github.com/Carcraftz/CycleTLS/cycletls"
+	http "github.com/Carcraftz/fhttp"
+
+	tls "github.com/Titanium-ctrl/utls"
 )
 
-var client = cycletls.Init()
+//var client http.Client
 
 func main() {
 	port := flag.String("port", "8082", "A port number (default 8082)")
 	flag.Parse()
 	addr := "127.0.0.1:" + string(*port)
 	fmt.Println("Hosting a TLS API on http://" + addr)
+	fmt.Println("If you like this API, all donations are appreciated! https://paypal.me/carcraftz")
 	http.HandleFunc("/", handleReq)
 	err := http.ListenAndServe(addr, nil)
 	if err != nil {
@@ -45,33 +55,45 @@ func handleReq(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Extract proxy from header
+	//Handle Proxy (http://host:port or http://user:pass@host:port)
 	proxy := r.Header.Get("Poptls-Proxy")
 	if proxy != "" {
 		r.Header.Del("Poptls-Proxy")
 	}
-
+	//handle redirects and timeouts
 	redirectVal := r.Header.Get("Poptls-Allowredirect")
 	allowRedirect := true
 	if redirectVal != "" {
 		if redirectVal == "false" {
-			fmt.Println(("redirects disabled"))
 			allowRedirect = false
 		}
 	}
 	if redirectVal != "" {
 		r.Header.Del("Poptls-Allowredirect")
 	}
-	// Change JA3
-	// Use Chrome JA3 by default
-	//TODO: ADD MORE JA3 HASHES
-	tlsClient := "771,4865-4866-4867-49195-49199-49196-49200-52393-52392-49171-49172-156-157-47-53,0-23-65281-10-11-35-16-5-13-18-51-45-43-27-21,29-23-24,0"
-	if strings.Contains(strings.ToLower(userAgent), "firefox") {
-		tlsClient = "771,4865-4867-4866-49195-49199-52393-52392-49196-49200-49162-49161-49171-49172-156-157-47-53-10,0-23-65281-10-11-35-16-5-51-43-13-45-28-21,29-23-24-25-256-257,0"
+	timeoutraw := r.Header.Get("Poptls-Timeout")
+	timeout, err := strconv.Atoi(timeoutraw)
+	if err != nil {
+		//default timeout of 6
+		timeout = 6
 	}
-
-	// Forward body
-	body, _ := ioutil.ReadAll(r.Body)
+	if timeout > 60 {
+		http.Error(w, "ERROR: Timeout cannot be longer than 60 seconds", http.StatusBadRequest)
+		return
+	}
+	// Change JA3
+	var tlsClient tls.ClientHelloID
+	if strings.Contains(strings.ToLower(userAgent), "chrome") {
+		tlsClient = tls.HelloChrome_Auto
+	} else if strings.Contains(strings.ToLower(userAgent), "firefox") {
+		tlsClient = tls.HelloFirefox_Auto
+	} else {
+		tlsClient = tls.HelloIOS_Auto
+	}
+	client, err := cclient.NewClient(tlsClient, proxy, allowRedirect, time.Duration(timeout))
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// Forward query params
 	var addedQuery string
@@ -85,48 +107,147 @@ func handleReq(w http.ResponseWriter, r *http.Request) {
 	} else if addedQuery != "" {
 		endpoint = pageURL + "?" + addedQuery[1:]
 	}
+	req, err := http.NewRequest(r.Method, ""+endpoint, r.Body)
+	if err != nil {
+		panic(err)
+	}
+	//master header order, all your headers will be ordered based on this list and anything extra will be appended to the end
+	//if your site has any custom headers, see the header order chrome uses and then add those headers to this list
+	masterheaderorder := []string{
+		"host",
+		"connection",
+		"cache-control",
+		"device-memory",
+		"viewport-width",
+		"rtt",
+		"downlink",
+		"ect",
+		"sec-ch-ua",
+		"sec-ch-ua-mobile",
+		"sec-ch-ua-full-version",
+		"sec-ch-ua-arch",
+		"sec-ch-ua-platform",
+		"sec-ch-ua-platform-version",
+		"sec-ch-ua-model",
+		"upgrade-insecure-requests",
+		"user-agent",
+		"accept",
+		"sec-fetch-site",
+		"sec-fetch-mode",
+		"sec-fetch-user",
+		"sec-fetch-dest",
+		"referer",
+		"accept-encoding",
+		"accept-language",
+		"cookie",
+	}
+	headermap := make(map[string]string)
+	//TODO: REDUCE TIME COMPLEXITY (This code is very bad)
+	headerorderkey := []string{}
+	for _, key := range masterheaderorder {
+		for k, v := range req.Header {
+			lowercasekey := strings.ToLower(k)
+			if key == lowercasekey {
+				headermap[k] = v[0]
+				headerorderkey = append(headerorderkey, lowercasekey)
+			}
+		}
+
+	}
+	for k, v := range req.Header {
+		if _, ok := headermap[k]; !ok {
+			headermap[k] = v[0]
+			headerorderkey = append(headerorderkey, strings.ToLower(k))
+		}
+	}
+
+	for k := range r.Header {
+		if k != "Content-Length" && !strings.Contains(k, "Poptls") {
+			req.Header.Set(k, r.Header.Get(k))
+		}
+	}
+	//ordering the pseudo headers and our normal headers
+	req.Header = http.Header{
+		http.HeaderOrderKey:  headerorderkey,
+		http.PHeaderOrderKey: {":method", ":authority", ":scheme", ":path"},
+	}
+	//set our Host header
 	u, err := url.Parse(endpoint)
 	if err != nil {
 		panic(err)
 	}
-	// Forward headers
-	headers := make(map[string]string)
-	for k := range r.Header {
-		if k != "Content-Length" {
-			headers[k] = r.Header.Get(k)
-		}
-	}
-	headers["Host"] = u.Host
-	// Set options
-	opts := cycletls.Options{
-		Body:          string(body), // ideally, body should be passed as bytes
-		Ja3:           tlsClient,
-		UserAgent:     userAgent,
-		Headers:       headers,
-		Method:        r.Method,
-		Proxy:         proxy,
-		AllowRedirect: allowRedirect,
-	}
-	// Perform request
-	res, err := client.Do(endpoint, opts, r.Method)
+	req.Header.Set("Host", u.Host)
+
+	resp, err := client.Do(req)
 	if err != nil {
-		log.Println("Request failed:", err)
+		panic(err)
 	}
-	fmt.Println(res.Response.Status)
-	// Forward response headers
-	for k, v := range res.Response.Headers {
-		// Do not forward the content length and encoding headers, as we will handle the decoding ourselves
+	defer resp.Body.Close()
+
+	//req.Close = true
+
+	//forward response headers
+	for k, v := range resp.Header {
 		if k != "Content-Length" && k != "Content-Encoding" {
-			w.Header().Set(k, v)
+			w.Header().Set(k, v[0])
 		}
-
 	}
-	// Forward respone status
-	w.WriteHeader(res.Response.Status)
+	w.WriteHeader(resp.StatusCode)
+	fmt.Println(resp.StatusCode)
 
-	// Write forwarded proxy request
-	if _, err := fmt.Fprint(w, res.Response.Body); err != nil {
+	//forward decoded response body
+	encoding := resp.Header["Content-Encoding"]
+	body, err := ioutil.ReadAll(resp.Body)
+	finalres := ""
+	if err != nil {
+		panic(err)
+	}
+	finalres = string(body)
+	if len(encoding) > 0 {
+		if encoding[0] == "gzip" {
+			unz, err := gUnzipData(body)
+			if err != nil {
+				panic(err)
+			}
+			finalres = string(unz)
+		} else if encoding[0] == "deflate" {
+			unz, err := enflateData(body)
+			if err != nil {
+				panic(err)
+			}
+			finalres = string(unz)
+		} else if encoding[0] == "br" {
+			unz, err := unBrotliData(body)
+			if err != nil {
+				panic(err)
+			}
+			finalres = string(unz)
+		} else {
+			fmt.Println("UNKNOWN ENCODING: " + encoding[0])
+			finalres = string(body)
+		}
+	} else {
+		finalres = string(body)
+	}
+	if _, err := fmt.Fprint(w, finalres); err != nil {
 		log.Println("Error writing body:", err)
 	}
+}
 
+func gUnzipData(data []byte) (resData []byte, err error) {
+	gz, _ := gzip.NewReader(bytes.NewReader(data))
+	defer gz.Close()
+	respBody, err := ioutil.ReadAll(gz)
+	return respBody, err
+}
+func enflateData(data []byte) (resData []byte, err error) {
+	zr, _ := zlib.NewReader(bytes.NewReader(data))
+	defer zr.Close()
+	enflated, err := ioutil.ReadAll(zr)
+	return enflated, err
+}
+func unBrotliData(data []byte) (resData []byte, err error) {
+	br := brotli.NewReader(bytes.NewReader(data))
+	respBody, err := ioutil.ReadAll(br)
+	return respBody, err
 }
